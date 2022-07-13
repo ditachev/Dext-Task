39d38834b628871888bcd3932e0e31cfc13ad37b
dimitar-t <dimitartachev23@gmail.com>
2022-07-13 19:31:58 +0300

    Changed RDS security group argument to VPC SG
    Turns out that there are two types of security groups for RDS instances.
    Up until now I was trying to use a DB security group which is meant for
    DBs in EC2-Classin instances.
    Changing to VPC SGs since this is what I am actually using.

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -28,13 +28,13 @@ resource "aws_security_group" "db_sg" {
 }
 
 resource "aws_db_instance" "rds" {
-  allocated_storage    = 10
-  engine               = var.db_engine
-  engine_version       = var.db_engine_version
-  instance_class       = var.db_instance_class
-  db_subnet_group_name = aws_db_subnet_group.rds_subnet_group.name
-  db_name              = var.db_name
-  username             = var.db_username
-  password             = var.db_password
-  security_group_names = [aws_security_group.db_sg.id]
+  allocated_storage      = 10
+  engine                 = var.db_engine
+  engine_version         = var.db_engine_version
+  instance_class         = var.db_instance_class
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
+  db_name                = var.db_name
+  username               = var.db_username
+  password               = var.db_password
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
 }
